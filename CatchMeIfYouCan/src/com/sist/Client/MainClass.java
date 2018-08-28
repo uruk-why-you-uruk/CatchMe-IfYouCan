c583c04a0a9a30ef6a0ddae6411f374a6ebe3ba2
iseul <iseul@Sist61>
2018-08-28 17:51:13 +0900

    하핳ㅎ

--- CatchMeIfYouCan/src/com/sist/Client/MainClass.java
+++ CatchMeIfYouCan/src/com/sist/Client/MainClass.java
@@ -3,10 +3,9 @@ package com.sist.Client;
 public class MainClass {
 
 	public static void main(String[] args) {
-		System.out.println("하하하로로ㅗ");
-		System.out.println("asdfa");
+		System.out.println("하하");
 	//github.com/uruk-why-you-uruk/CatchMe-IfYouCan.git
-		System.out.println("안녕히 가세요");
+	
 
 	}
 

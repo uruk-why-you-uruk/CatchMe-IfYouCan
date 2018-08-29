07e4b6d994b29d11a0ada112d5ae69733b4532e3
iseul <iseul@Sist61>
2018-08-29 17:22:56 +0900

    game

--- CatchMeIfYouCan/src/com/sist/Client/Catch_gameroom.java
+++ CatchMeIfYouCan/src/com/sist/Client/Catch_gameroom.java
@@ -1,18 +1,21 @@
 package com.sist.Client;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 class char_if
 {
 	JLabel id,rank,score,icon;
 }
 
-public class Catch_gameroom extends JPanel{
+public class Catch_gameroom extends JPanel implements ActionListener{
 	Image back;
 	JPanel draw,timer;
 	JLabel room_grade,chat;
 	char_if[] player = new char_if[8];
 	JPanel[] char_group = new JPanel[8];
+	JTextArea ta;
 	JTextField tf;
 	JButton[] color = new JButton[8];
 	
@@ -20,6 +23,11 @@ public class Catch_gameroom extends JPanel{
 		setLayout(null);
 		back = Toolkit.getDefaultToolkit().getImage("image\\gamm.jpg");
 
+		
+		ta = new JTextArea();
+	    JScrollPane js3 = new JScrollPane(ta);
+	    
+	    tf = new JTextField();
 		//초기값
 		
 		for(int i=0;i<8;i++)
@@ -57,21 +65,27 @@ public class Catch_gameroom extends JPanel{
 		add(draw);
 		add(tf);
 		add(timer);
+		add(ta);
 		
-		char_group[0].setBounds(50, 130, 180, 110);
-		char_group[1].setBounds(50, 250, 180, 110);
-		char_group[2].setBounds(50, 370, 180, 110);
-		char_group[3].setBounds(50, 490, 180, 110);
-	    char_group[4].setBounds(1025, 130, 180, 110);
-	    char_group[5].setBounds(1025, 250, 180, 110);
-	    char_group[6].setBounds(1025, 370, 180, 110);
-	    char_group[7].setBounds(1025, 490, 180, 110);
-	    draw.setBounds(265, 130, 725, 320);
-	    timer.setBounds(265, 520, 120, 80);
-	    tf.setBounds(700, 560, 300, 30);
+		char_group[0].setBounds(50, 110, 180, 110);
+		char_group[1].setBounds(50, 230, 180, 110);
+		char_group[2].setBounds(50, 350, 180, 110);
+		char_group[3].setBounds(50, 470, 180, 110);
+	    char_group[4].setBounds(1025, 110, 180, 110);
+	    char_group[5].setBounds(1025, 230, 180, 110);
+	    char_group[6].setBounds(1025, 350, 180, 110);
+	    char_group[7].setBounds(1025, 470, 180, 110);
+	    
+	    draw.setBounds(265, 110, 725, 370);
+	    timer.setBounds(265, 600, 150, 50);
+	    ta.setBounds(700, 500, 290, 100);
+	    ta.setOpaque(true);
+	    tf.setBounds(700, 620, 290, 30);
 		
+	    
 		setVisible(true);
 		
+		tf.addActionListener(this);
 	}
 	 @Override
 	   protected void paintComponent(Graphics g) {
@@ -79,6 +93,14 @@ public class Catch_gameroom extends JPanel{
 
 	   }
 
+	 @Override
+	   public void actionPerformed(ActionEvent e) { // 채팅을치면 채팅창에 입력된게 올라가는고
+	      if (e.getSource() == tf) {
+	         String s = tf.getText();
+	         ta.append(s + "\n");
+	         tf.setText("");
+	      }
+	   }
 	
 
 

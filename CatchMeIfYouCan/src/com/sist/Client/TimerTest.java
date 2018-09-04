bbd4ee19acddcb38c271f1c5fcd6dee5445c1014
iseul <iseul@Sist61>
2018-09-04 16:33:35 +0900

    timer 종료

--- CatchMeIfYouCan/src/com/sist/Client/TimerTest.java
+++ CatchMeIfYouCan/src/com/sist/Client/TimerTest.java
@@ -1,8 +1,7 @@
 package com.sist.Client;
-import java.awt.*;
-import javax.swing.*;
 
- 
+import javax.swing.*;
+import java.awt.*;
 
 public class TimerTest extends JFrame{
     JLabel timerLabel = null;
@@ -22,7 +21,7 @@ public class TimerTest extends JFrame{
         int k = 150;
         while(k!=-1)
         {
-        	int minutes = k / 60;
+           int minutes = k / 60;
             int seconds = k % 60;
             timerLabel.setText(String.valueOf(String.format("%02d:%02d",minutes, seconds)));
             try {
@@ -32,12 +31,11 @@ public class TimerTest extends JFrame{
             }
             k--;
         }
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
     public static void main(String[] args) {
         new TimerTest();
 
     }
-
-}
-
+   }

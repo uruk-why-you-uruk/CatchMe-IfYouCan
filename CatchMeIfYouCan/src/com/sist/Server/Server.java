e20d600c75bf3ce0000512e91539bd0740a532d5
user <user@user-pc>
2018-09-07 15:27:34 +0900

    server update

---
+++ CatchMeIfYouCan/src/com/sist/Server/Server.java
@@ -0,0 +1,77 @@
+package com.sist.Server;
+import java.util.*;
+import java.io.*;
+import java.net.*;
+public class Server implements Runnable{
+	ServerSocket ss;
+	Vector<Client> waitVc=new Vector<Client>(); 
+	public Server()
+	{
+		try
+		{
+			ss=new ServerSocket(3355);
+			System.out.println("서버 가동...");
+		}catch(Exception ex){
+			System.out.println(ex.getMessage());
+		}
+	}
+	public void run()
+	{
+		try
+		{
+			while(true)
+			{
+				Socket s=ss.accept();
+				Client client=new Client(s);
+				waitVc.addElement(client);
+				client.start();
+				System.out.println("client접속완료");
+			}
+		}catch(Exception ex){}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Server server=new Server();
+        new Thread(server).start();
+	}
+	
+    class Client extends Thread
+    {
+    	Socket s;
+    	OutputStream out;
+    	
+    	BufferedReader in;
+    	public Client(Socket s)
+    	{
+    		try
+    		{
+    		   this.s=s;
+    		   out=s.getOutputStream(); // 
+    		   in=new BufferedReader(
+    				   new InputStreamReader(
+    						   s.getInputStream()));
+    		}catch(Exception ex){}
+    	}
+    	public void run()
+    	{
+    		try
+    		{
+    			while(true)
+    			{
+    				
+    				String msg=in.readLine();
+    				System.out.println(msg);
+    				
+    				for(int i=0;i<waitVc.size();i++)
+    				{
+    					Client user=waitVc.elementAt(i);
+    					if(user!=this)
+    					{
+    					  user.out.write((msg+"\n").getBytes());
+    					}
+    				}
+    			}
+    		}catch(Exception ex){}
+    	}
+    }
+}

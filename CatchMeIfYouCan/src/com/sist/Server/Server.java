c5c1e31d86d0e6a1847f28cb707226a557bdcaeb
user <user@user-pc>
2018-09-13 14:27:43 +0900

    ㅁㄴㅇㄹ

--- CatchMeIfYouCan/src/com/sist/Server/Server.java
+++ CatchMeIfYouCan/src/com/sist/Server/Server.java
@@ -7,66 +7,6 @@ import com.sist.common.*;
 import java.io.*;
 import java.net.*;
 
-public class Server implements Runnable{
-   ServerSocket ss;
-   Vector<Client> waitVc=new Vector<Client>(); 
-   public Server()
-   {
-      try
-      {
-         ss=new ServerSocket(7339);
-         System.out.println("서버 가동...");
-      }catch(Exception ex){
-         System.out.println(ex.getMessage());
-      }
-   }
-   public void run()
-   {
-      try
-      {
-         while(true)
-         {
-            Socket s=ss.accept();
-            Client client=new Client(s);
-            waitVc.addElement(client);
-            client.start();
-            System.out.println("client접속완료");
-         }
-      }catch(Exception ex){}
-   }
-   public static void main(String[] args) {
-      // TODO Auto-generated method stub
-        Server server=new Server();
-        new Thread(server).start();
-   }
-   
-    class Client extends Thread
-    {
-       Socket s;
-       OutputStream out;       
-       BufferedReader in;
-       public Client(Socket s)
-       {
-          try
-          {
-             this.s=s;
-             out=s.getOutputStream(); // 
-             in=new BufferedReader(
-                   new InputStreamReader(
-                         s.getInputStream()));
-          }catch(Exception ex){}
-       }
-       public void run()
-       {
-          try
-          {
-             while(true)
-             {
-                
-                String msg=in.readLine();
-                StringTokenizer st=new StringTokenizer(msg, "|");
-                   int no=Integer.parseInt(st.nextToken());
-
 public class Server implements Runnable {
 	ServerSocket ss;
 	Vector<Client> waitVc = new Vector<Client>();
